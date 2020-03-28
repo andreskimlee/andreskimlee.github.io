927850f8a62bc06f1b5b5ff4658e262a1d1515ce
Andres Kim <andreskimlee@gmail.com>
2020-03-28 03:22:06 -0400

    first commit

---
+++ precache-manifest.6fdc78a6bf157588e028be2efdb870f3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37a1f9757369465c621d1d6472cae3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1be70f81e0730ec8947",
+    "url": "/static/css/2.dd1866a9.chunk.css"
+  },
+  {
+    "revision": "f5c55ac2f4bc980b65c8",
+    "url": "/static/css/main.dd833361.chunk.css"
+  },
+  {
+    "revision": "f1be70f81e0730ec8947",
+    "url": "/static/js/2.df832608.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df832608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c55ac2f4bc980b65c8",
+    "url": "/static/js/main.f9e8794b.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "6002505aa00f7179f7396e87fdbfde23",
+    "url": "/static/media/AndresKimLee.6002505a.pdf"
+  },
+  {
+    "revision": "8d48552b0e3b90fc79b5091bb267001b",
+    "url": "/static/media/CalibreBlack.8d48552b.otf"
+  },
+  {
+    "revision": "41e0aa46db4d8da5744be6fca83e68dd",
+    "url": "/static/media/Github.41e0aa46.svg"
+  },
+  {
+    "revision": "59d4e824f1b9c08e537ca3e1a45c7c8a",
+    "url": "/static/media/angel.59d4e824.svg"
+  },
+  {
+    "revision": "9bfc5f70f252be188ebe440f63ec4493",
+    "url": "/static/media/folder.9bfc5f70.svg"
+  },
+  {
+    "revision": "7cbb5b98e1ac6d39923050de674501cc",
+    "url": "/static/media/funny-comp.7cbb5b98.gif"
+  },
+  {
+    "revision": "ccaacdf0cea130f826466c70edf35272",
+    "url": "/static/media/goTo.ccaacdf0.svg"
+  },
+  {
+    "revision": "fe93bbb5331a23563ab9b6cb8e1e3d77",
+    "url": "/static/media/linkedIn.fe93bbb5.svg"
+  },
+  {
+    "revision": "783ffe83171b77b0e2ad8d5abd6ff116",
+    "url": "/static/media/loading.783ffe83.gif"
+  },
+  {
+    "revision": "71469e6be5991912ebd25f5a320886ea",
+    "url": "/static/media/solvent.71469e6b.svg"
+  }
+]);
